fd71e1ecc6269e48095b94b30c37d7d53fba201f
Martin Franek <martin.franek@cdn77.com>
2022-04-29 13:13:59 +0200

    spravna inicializace a iterace v poli karticek

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,37 +1,42 @@
-import React from 'react';
-import { Card } from './Card/Card';
+import React from "react";
 
-import '../styles/normalize.css';
-import '../styles/global.css';
+import "../styles/normalize.css";
+import "../styles/global.css";
 
 export const App = () => {
-	const cardsArr = [];
-	// CREATE ARRAY OF CARD OJECTS
-	for (let i = 1; i < 9; i++) {
-		cardsArr.push({
-			id: `mario${i}`,
-		//	img: `images/${i}.png`,
-			isFliped: false,
-			isDisabled: false,
-			isPaired: false,
-		});
-	}
+  const [cards, setCards] = React.useState();
 
-	console.log(cardsArr.id);
+  React.useEffect(() => {
+    // vytvor pole s kartickami pouze jednou pri startu aplikace
+    // toto je extremne dulezite! ze se musi vytvorit pouze jednou a smitec
+    setCards(
+      [...Array(8)].map((_, index) => ({
+        id: `mario-${index}`,
+        src: `/images/mario${index}.png`,
+        isFliped: false,
+        isDisabled: false,
+        isPaired: false,
+      }))
+    );
+  }, []);
 
-	return (
-		<div className="container">
-			<img
-				className="testImg"
-				src={`images/${cardsArr.id}.png`}
-				alt="Memory Game Card"
-			></img>
-
-			{cardsArr.map((card, index) => (
-				<Card />
-			))}
-		</div>
-	);
+  return (
+    <div className="container">
+      {/* dokud neni pole s kartami definovano, zobraz napis loading, jinak iteruj */}
+      {cards ? (
+        cards.map((card) => (
+          <img
+            key={card.id}
+            className="testImg"
+            width={100}
+            height={100}
+            src={card.src}
+            alt="Memory Game Card"
+          />
+        ))
+      ) : (
+        <p>Loading</p>
+      )}
+    </div>
+  );
 };
-
-//`images/${cardsArr.id}.png`
