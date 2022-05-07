c80a9fa6854d417e04f7c2c5d6aa6848b3e88746
Misch@ <Michaela.pupikova22@gmail.com>
2022-05-07 18:36:31 +0200

    zmeny

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
-import "../styles/normalize.css";
-import "../styles/global.css";
+import '../styles/normalize.css';
+import '../styles/global.css';
 
 export const App = () => {
   const [cards, setCards] = React.useState();
@@ -14,25 +14,25 @@ export const App = () => {
       [...Array(8)].map((_, index) => ({
         id: `mario-${index}`,
         src: `/images/mario${index}.png`,
-        isFliped: false,
-        isDisabled: false,
-        isPaired: false,
+        isFliped: true,
+        isDisabled: true,
+        isPaired: true,
       }))
     );
   }, []);
 
   return (
-    <div className="container">
+    <div className='container'>
       {/* dokud neni pole s kartami definovano, zobraz napis loading, jinak iteruj */}
       {cards ? (
         cards.map((card) => (
           <img
             key={card.id}
-            className="testImg"
+            className='testImg'
             width={100}
             height={100}
             src={card.src}
-            alt="Memory Game Card"
+            alt='Memory Game Card'
           />
         ))
       ) : (
